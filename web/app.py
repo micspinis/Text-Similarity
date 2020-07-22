0c241e71dab0b179f9de6f756e303ed2c3e393b3
MiguelSomohano <miguel.spinia@gmail.com>
2020-07-21 22:04:54 -0500

    Adding refill to app.py and ready to first docker-up

--- web/app.py
+++ web/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
-
+import spacy
 
 app = Flask(__name__)
 api = Api(app)
@@ -48,3 +48,140 @@ class Register(Resource):
             "msg": "You've successfully signed up to the API"
         }
         return jsonify(retJson)
+
+def verifyPw(username, password):
+    if not UserExist(username):
+        return False
+
+    hashed_pw = users.find({
+        "Username": username
+    })[0]["Password"]
+    
+    if bcrypt-hashpw(password.enconde("utf8"), hashed_pw) == hashed_pw:
+        return True
+    else:
+        return False
+    
+
+def countTokens(username):
+   tokens = user.find({
+       "Username": username
+       })[0]["Tokens"]
+   return tokens
+
+class Detect(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+        text1 = postedData["text1"]
+        text2 = postedData["text2"]
+            
+        if not UserExist(username):
+            retJson = {
+                "status": 301,
+                "msg": "Invalid Username"
+            }
+            return jsonify(retJson)
+
+        correct_pw = verifyPw(username, password)
+
+        if not correct_pw:
+            retJson = {
+                "status": 302,
+                "msg": "Invalid Password"
+            }
+            return jsonify(retJson)
+
+
+        num_tokens = countTokens(username)
+
+        if num_tokens <= 0:
+            retJson = {
+                "status": 303,
+                "msg": "You're out of tokens, please refill!"
+            }
+            return jsonify(retJson)
+
+
+        #Calculate the edit distance
+        nlp = spacy.load("en_core_web_sm") 
+
+        text1 = nlp(text1)
+        text2 = nlp(text2)
+
+        #Ratio is a number between 0 and 1 the closer to 1 , the more similar text1and text2 are
+        ratio = text1.similarity(text2)
+
+        retJson = {
+            "status": 200,
+            "similarity": ratio,
+            "msg": "Similarity score calculated successfully"
+        }
+
+
+        current_tokens = countTokens(username)
+
+        user.update({
+            "Username": username,
+        }, {
+            "$set":{
+                "Tokens": current_tokens - 1
+                }
+        })
+
+        return jsonify(retJson)
+
+class Refill(Resource):
+    def post(self):
+        postedData = request.get_json()
+        
+        username = postedData["username"]
+        password = postedData["admin_pw"]
+
+        refill_amount = postedData["refill"]
+        
+        if not UserExist(username):
+            retJson = {
+                "status": 301,
+                "msg": "Invalid Username"
+            }
+            return jsonify(retJson)
+
+        correct_pw = "abc123" #Contraseña de administrador, solo de prueba 
+
+        if not password == correct_pw:
+            retJson = {
+                "status": 304,
+                "msg": "Invalid Admin Password"
+            }
+            return jsonify(retJson)
+
+#        current_tokens =  countTokens(username)
+        users.update({
+            "Username": username
+        }, {
+            "$set": {
+                "Tokens": refill_amount #+ current_tokens
+            }
+        })
+
+
+        retJson = {
+            "status": 200,
+            "msg": "Refill Successfully"
+        }
+        return jsonify(retJson)
+
+
+#URLs
+api.add_resource(Register, "/register")
+api.add_resource(Detect, "/detect")
+api.add_resource(Refill, "/refill")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
